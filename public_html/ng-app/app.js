b87d83d147e6bed13c42aeb8d5399503997555a8
Mikko Karaiste <mikkara@gmail.com>
2016-07-27 12:39:52 +0300

    ng-module from index.html inserted into app.js file

--- public_html/ng-app/app.js
+++ public_html/ng-app/app.js
@@ -2,3 +2,40 @@
  * ng-scripts will be added here later from index.html
  */
 
+var app = angular.module('app', ['ngRoute', 'ngResource']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+            .when('/templateOne', {
+                templateUrl: './templates/templateOne.html',
+                controller: 'templateOneController'
+            })
+
+            .when('/templateTwo', {
+                templateUrl: './templates/templateTwo.html',
+                controller: 'templateTwoController'
+            })
+
+            .otherwise({
+                templateUrl: './templates/templateMain.html',
+                controller: 'templateMainController'
+            });
+
+});
+
+app.controller('templateOneController', ['$scope', function ($scope) {
+        $scope.title = 'templateOne working!';
+    }]);
+
+app.controller('templateTwoController', ['$scope', function ($scope) {
+        $scope.title = 'templateTwo working!';
+    }]);
+
+app.controller('templateMainController', ['$scope', function ($scope) {
+        $scope.title = 'templateMain working!';
+    }]);
+
+app.controller('mainController', ['$scope', function ($scope) {
+        $scope.title = 'ng working!';
+    }]);
