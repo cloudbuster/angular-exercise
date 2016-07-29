1d7c44a5787c956b2c5ba8e80320b7d76a14efb8
Mikko Karaiste <mikkara@gmail.com>
2016-07-29 13:39:40 +0300

    Added service to store user input. User input is printed out with ng-route.

--- public_html/ng-app/app.js
+++ public_html/ng-app/app.js
@@ -2,63 +2,69 @@
  * ng-scripts will be added here later from index.html
  */
 
-
 var app = angular.module('app', ['ngRoute', 'ngResource']);
 
 app.config(function ($routeProvider) {
     $routeProvider
 
-            .when('/templateOne', {
-                templateUrl: './templates/templateOne.html',
-                controller: 'templateOneController'
-            })
+    .when('/templateOne', {
+        templateUrl: './templates/templateOne.html',
+        controller: 'templateOneController'
+    })
 
-            .when('/templateTwo', {
-                templateUrl: './templates/templateTwo.html',
-                controller: 'templateTwoController'
-            })
+    .when('/templateTwo', {
+        templateUrl: './templates/templateTwo.html',
+        controller: 'templateTwoController'
+    })
 
-            .otherwise({
-                templateUrl: './templates/templateMain.html',
-                controller: 'templateMainController'
-            });
+    .otherwise({
+        templateUrl: './templates/templateMain.html',
+        controller: 'templateMainController'
+    });
 
 });
 
-app.controller('templateOneController', ['$scope', function ($scope) {
-	$scope.title = 'templateOne working!';
-	
-	$scope.register = function(){
-		console.log("Success");	
-		console.log($scope.reg);
-		// JSON stringiksi (localStorage("key", JSON.stringify(myJson)), 
-		// stringi JSONiksi JSON.parse(localStorage(getItem('person');
-		console.log(JSON.stringify($scope.reg));
-		localStorage("person", JSON.stringify($scope.reg));
-	};
+app.service('myService', function(){
+    this.tempStorage = new Array();
+});
+
+app.controller('templateOneController', ['$scope', 'myService', function ($scope, myService) {
+    $scope.title = 'templateOne working!';
+    
+    $scope.register = function(){
+        // JSON stringiksi (localStorage("key", JSON.stringify(myJson)), 
+        // stringi JSONiksi JSON.parse(localStorage(getItem('person');
+        myService.tempStorage.push($scope.reg);
+
+        console.log("$scope.reg:" + JSON.stringify($scope.reg));
+        console.log("myService.tempStorage: " + JSON.stringify(myService.tempStorage));
+    };
 
 }]);
 
-app.controller('templateTwoController', ['$scope', '$resource', '$http', function ($scope, $resource, $http) {
-        $scope.title = 'templateTwo working!';
+app.controller('templateTwoController', ['$scope', '$resource', '$http', 'myService', function ($scope, $resource, $http, myService) {
+    $scope.title = 'templateTwo working!';
 
-	var onDataLoaded = function (response) {
-		$scope.persons = response.data;
-		console.log("Success!");
-	};
+    var onDataLoaded = function (response) {
+        $scope.persons = response.data;
+        console.log("Success!");
+    };
 
-	var onError = function (reason) {
-		console.log("Didn't work. Error." + reason.data);
-	};
+    var onError = function (reason) {
+        console.log("Didn't work. Error." + reason.data);
+    };
 
-	$http.get('./api/list.json')
-		.then(onDataLoaded, onError);
+    $http.get('./api/list.json')
+            .then(onDataLoaded, onError);
+
+    $scope.fromService = myService.tempStorage;
+    console.log("***** tempStorage :" + myService.tempStorage[0].firstName);
+}]);
+    
+app.controller('templateMainController', ['$scope', function ($scope) {
+    $scope.title = 'templateMain working!';
 }]);
 	
-	app.controller('templateMainController', ['$scope', function ($scope) {
-			$scope.title = 'templateMain working!';
-		}]);
-	
-	app.controller('mainController', ['$scope', function ($scope) {
-			$scope.title = 'ng working!';
-		}]);
+app.controller('mainController', ['$scope', function ($scope) {
+    $scope.title = 'ng working!';
+}]);
