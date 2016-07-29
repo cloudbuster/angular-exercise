cbe93ca94ec26f0b98acdc17105e77a612ed6b90
Mikko Karaiste <mikkara@gmail.com>
2016-07-29 03:39:03 +0300

    Added http.get to get data and ng-repeat to present data. TODO: localStorage

--- public_html/ng-app/app.js
+++ public_html/ng-app/app.js
@@ -2,6 +2,7 @@
  * ng-scripts will be added here later from index.html
  */
 
+
 var app = angular.module('app', ['ngRoute', 'ngResource']);
 
 app.config(function ($routeProvider) {
@@ -25,17 +26,39 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('templateOneController', ['$scope', function ($scope) {
-        $scope.title = 'templateOne working!';
-    }]);
+	$scope.title = 'templateOne working!';
+	
+	$scope.register = function(){
+		console.log("Success");	
+		console.log($scope.reg);
+		// JSON stringiksi (localStorage("key", JSON.stringify(myJson)), 
+		// stringi JSONiksi JSON.parse(localStorage(getItem('person');
+		console.log(JSON.stringify($scope.reg));
+		localStorage("person", JSON.stringify($scope.reg));
+	};
 
-app.controller('templateTwoController', ['$scope', function ($scope) {
+}]);
+
+app.controller('templateTwoController', ['$scope', '$resource', '$http', function ($scope, $resource, $http) {
         $scope.title = 'templateTwo working!';
-    }]);
 
-app.controller('templateMainController', ['$scope', function ($scope) {
-        $scope.title = 'templateMain working!';
-    }]);
+	var onDataLoaded = function (response) {
+		$scope.persons = response.data;
+		console.log("Success!");
+	};
 
-app.controller('mainController', ['$scope', function ($scope) {
-        $scope.title = 'ng working!';
-    }]);
+	var onError = function (reason) {
+		console.log("Didn't work. Error." + reason.data);
+	};
+
+	$http.get('./api/list.json')
+		.then(onDataLoaded, onError);
+}]);
+	
+	app.controller('templateMainController', ['$scope', function ($scope) {
+			$scope.title = 'templateMain working!';
+		}]);
+	
+	app.controller('mainController', ['$scope', function ($scope) {
+			$scope.title = 'ng working!';
+		}]);
